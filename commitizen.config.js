1893e768eda0eb74f5414893434a42c03ea497d9
i0ji <ioji@inbox.ru>
2025-12-07 01:18:44 +0300

    build(full product): deps update

--- commitizen.config.js
+++ commitizen.config.js
@@ -1,64 +1,59 @@
 "use strict";
 
 module.exports = {
-  types: [
-    {
-      value: "build",
-      name: "build:     out builds",
-    },
-    {
-      value: "chore",
-    },
-    {
-      value: "ci",
-      name: "ci/cd",
-    },
-    {
-      value: "docs",
-      name: "docs:      docs update",
-    },
-    {
-      value: "feat",
-      name: "feat:      add new features",
-    },
-    { value: "fix", name: "fix:       fix bugs" },
-    {
-      value: "refactor",
-    },
-    {
-      value: "revert",
-      name: "revert:    reset/revert commit",
-    },
-    {
-      value: "style",
-      name: "style:     style/implementation features",
-    },
-    { value: "tests" },
-  ],
+    types: [
+        {
+            value: "build",
+            name: "build:     out builds",
+        },
+        {
+            value: "chore",
+        },
+        {
+            value: "ci",
+            name: "ci/cd",
+        },
+        {
+            value: "docs",
+            name: "docs:      docs update",
+        },
+        {
+            value: "feat",
+            name: "feat:      add new features",
+        },
+        {value: "fix", name: "fix:       fix bugs"},
+        {
+            value: "refactor",
+        },
+        {
+            value: "revert",
+            name: "revert:    reset/revert commit",
+        },
+        {
+            value: "style",
+            name: "style:     style/implementation features",
+        },
+        {value: "tests"},
+    ],
 
-  scopes: [
-    { name: "components" },
-    { name: "environment" },
-    { name: "full product" },
-  ],
+    scopes: [{name: "components"}, {name: "environment"}, {name: "full product"}],
 
-  messages: {
-    type: "Changes type:",
-    scope: "\nChoose scope:",
-    customScope: "Custom scope:",
-    subject: "Short description:\n",
-    body: 'Full description. Use "|" for new row:\n',
-    breaking: "Breaking changes:\n",
-    footer:
-      "Meta (issues, links):\n",
-    confirmCommit: "Confirm",
-  },
+    messages: {
+        type: "Changes type:",
+        scope: "\nChoose scope:",
+        customScope: "Custom scope:",
+        subject: "Short description:\n",
+        body: 'Full description. Use "|" for new row:\n',
+        breaking: "Breaking changes:\n",
+        footer: "Meta (issues, links):\n",
+        confirmCommit: "Confirm",
+    },
 
-  allowCustomScopes: true,
+    allowCustomScopes: true,
 
-  allowBreakingChanges: false,
+    allowBreakingChanges: false,
 
-  footerPrefix: "Version:",
+    footerPrefix: "Version:",
 
-  subjectLimit: 72,
+    subjectLimit: 72,
 };
